c4957dc8ecf774bb9fff55ce6ee656f42793a822
Dotsworthy <dotsworthy@gmail.com>
2020-03-23 20:55:42 +0000

    completed MVP

--- src/main/java/WaterBottle.java
+++ src/main/java/WaterBottle.java
@@ -1,2 +1,19 @@
 public class WaterBottle {
+    private int volume;
+
+    public WaterBottle(int volume) {
+    this.volume = volume;
+    }
+
+    public int Drink() {
+       return this.volume -= 10;
+    }
+
+    public int empty() {
+        return this.volume = 0;
+    }
+
+    public int fill() {
+        return this.volume = 100;
+    }
 }
--- src/main/java/WaterBottleTest.java
+++ src/main/java/WaterBottleTest.java
@@ -1,2 +1,20 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class WaterBottleTest {
+    WaterBottle waterBottle;
+
+    @Before
+    public void before() {waterBottle = new WaterBottle(100);}
+
+    @Test
+    public void canDrink() {assertEquals(90, waterBottle.Drink());}
+
+    @Test
+    public void canEmpty() {assertEquals(0,waterBottle.empty());}
+
+    @Test
+    public void canFill() {assertEquals(100, waterBottle.fill());}
 }
